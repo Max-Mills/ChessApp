3205fdcf2ae47920661bab696132c914d371bc78
Max <max33134@gmail.com>
2021-12-24 16:21:18 -0500

    Added Auth0
    Attempting to add Auth0 to the application

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
     <router-link to="/chess">Chess Game</router-link> |
     <router-link to="/about">About</router-link>
   </div>
-  <router-view/>
 </template>
 
 <style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,6 +2,12 @@
   <div class="hello">
     <h2> Welcome to Chess Online </h2>
     <p> To get started go to Chess Game </p>
+    <div v-if="!$auth.loading">
+      <!-- show login when not authenticated -->
+      <button v-if="!$auth.isAuthenticated" @click="login">Log in</button>
+      <!-- show logout when authenticated -->
+      <button v-if="$auth.isAuthenticated" @click="logout">Log out</button>
+    </div>
   </div>
 </template>
 
@@ -10,6 +16,18 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  methods: {
+    // Log the user in
+    login() {
+      this.$auth.loginWithRedirect();
+    },
+    // Log the user out
+    logout() {
+      this.$auth.logout({
+        returnTo: window.location.origin
+      });
+    }
   }
 }
 </script>
